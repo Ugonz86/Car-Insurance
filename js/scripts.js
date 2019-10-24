e4c423e39b19464824116158e8bc7240b850482e
Trevor Mackin and Uriel Gonzalez <uriel@>
2019-10-24 13:13:43 -0700

    Add background color and margins.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,15 @@ $(document).ready(function() {
 
     if (age) {
       var quote = (100 - age) * 3;
-      if (gender === 'male' && age < 26) {
+      if (gender === 'male' || age < 26) {
         quote += 50;
-        
       }
-
       $("#rate").empty().append(quote);
       $("#quote").show();
-    } else {
-      alert('Please enter your age.');
     }
+      else {
+        alert('Please enter your age.');
+      }
 
     event.preventDefault();
   });
